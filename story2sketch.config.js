dbbb72e45a8d542e4a6a7c2db540668c2551cb20
Miroslav Simulcik <simulcik.miro@gmail.com>
2018-04-28 18:00:12 +0200

    Fixed travis and removed viewport

--- story2sketch.config.js
+++ story2sketch.config.js
@@ -2,7 +2,6 @@ module.exports = {
   output: "sketch/page.asketch.json",
   stories: "all",
   viewports: {
-    Mobile: { width: 320, height: 1200 },
     Desktop: { width: 1920, height: 1200 }
   },
   outputDoc: "sketch/document.asketch.json"
