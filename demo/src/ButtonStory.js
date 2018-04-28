5b777b4bca83a1fcd01624b3fdbfa03b7642b420
Miroslav Simulcik <simulcik.miro@gmail.com>
2018-04-28 15:47:35 +0200

    Fixed button text

--- demo/src/ButtonStory.js
+++ demo/src/ButtonStory.js
@@ -12,7 +12,7 @@ stories.add("simple", () => (
 
 stories.add("simple2", () => (
   <Box backgroundColor="red" display="inline-block" p="small">
-    <Text color="blue">Hello Button</Text>
+    <Text color="blue">Hellooo Button</Text>
   </Box>
 ));
 
